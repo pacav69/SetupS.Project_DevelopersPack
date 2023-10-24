d64715dc9da9cf3262486d1f3ce0c16a8088f9a7
PaulCav <pacav69@gmail.com>
2023-10-24 17:50:15 +1100

    modified:   makehelp.cmd

--- makehelp.cmd
+++ makehelp.cmd
@@ -3,6 +3,8 @@
 @REM make help script for upload
 cls
 color 0a
+@REM debug
+@REM goto confirmdelhelp
 
 @REM #######################################################
 @REM ###  WebLink1=sstek.vergitek.com
@@ -62,3 +64,44 @@ type %helpfilesini1%
 @REM this will upload the files to the contents of  %helpfilesini1%
 @REM call uploadfilesftp.cmd <filename>
 call uploadfilesftp.cmd %helpfilesini1%
+
+goto confirmdelhelp
+
+
+@REM #######################################################
+@REM ###  confirmdelhelp
+@REM #######################################################
+:confirmdelhelp
+@REM https://www.robvanderwoude.com/choice.php
+CHOICE /C:YN /N /T 5 /D N  /M "Do you want to DELETE help files, default is No ['Y'es/'N'o] : "
+if errorlevel 2 goto :somewhere_else
+if errorlevel 1 goto :somewhere
+echo.
+:somewhere
+@REM echo "I am here because you typed Y"
+set deletehelp=Yes
+echo.
+
+Echo %_bBWhite%%_bBlue%####################################################### %_fBGreen%%_bBlack%
+echo Help Files will be deleted
+echo.
+
+timeout /T 5
+goto deletehelp
+
+:somewhere_else
+@REM echo "I am here because you typed N"
+set deletehelp=No
+goto exit
+
+@REM #######################################################
+@REM ###  deletehelp
+@REM #######################################################
+:deletehelp
+if exist "%~dp0%sc%\htmlhelp" rd /s /q "%~dp0%sc%\htmlhelp" >nul:
+
+@REM #######################################################
+@REM ###  exit
+@REM #######################################################
+:exit
+
